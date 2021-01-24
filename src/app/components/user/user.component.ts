b17caed4565bbbb22c0a4bfde8a5ba4b4777ebea
Ely Bourland <JEBourland@yahoo.com>
2021-01-24 10:21:39 -0800

    cleanup with interface

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,73 +1,29 @@
 import { Component } from '@angular/core';
 
+import { User } from '../../models/User';
+
 @Component({
     selector: 'app-user',
     templateUrl: './user.component.html',
     styleUrls: ['./user.component.css']
-    // template: '<h2>John Doe</h2>'
-    // styles: [`
-    //     h2 {
-    //         color: blue
-    //     }
-    // `]
 })
 
 export class UserComponent {
     // Properties
-    firstName: string;
-    lastName: string;
-    age: number;
-    address;
-
-    foo: any;
-    hasKids: boolean;
-    numberArray: number[];
-    stringArray: string[];
-    mixedArray: any[];
-    myTupal: [string, number, boolean];
-    unusable: void;
-    u: undefined;
-    n: null;
+    user: User;
 
     // Methods
     constructor() {
-        this.firstName = 'John';
-        this.lastName = 'Doe';
-        this.age = 30;
-
-        this.address = {
-            street: '50 Main St',
-            city: 'Boston',
-            state: 'MA'
-        }
-
-        this.foo = true;
-        this.hasKids = true;
-        this.numberArray = [1, 2, 3];
-        this.stringArray = ['hello', 'world'];
-        this.mixedArray = [true, undefined, 'hello', 3];
-        this.myTupal = ['hello', 1, true];
-        this.unusable = undefined;
-        this.u = undefined;
-        this.n = null;
-
-        console.log(this.addNumbers(2, 3));
-        
+      this.user = {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+              street: '50 Main St',
+              city: 'Boston',
+              state: 'MA'
+          }
+      }  
     }
-
-    showAge() {
-        return this.age + 2;
-    }
-
-    addNumbers(num1: number, num2: number): number {
-        return num1 + num2;
-    }
-
-    // sayHello() {
-    //     console.log(`Hello ${this.firstName}`)
-    // }
-
-    // hasBirthday() {
-    //     this.age += 1;
-    // }
 }
+
